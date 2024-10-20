497ee6f5be3a7b9fd6182059b08e0bdfabee6d0a
Almohandis <67489100+Almohandis@users.noreply.github.com>
2024-10-21 00:57:11 +0300

    admin controller refactor

--- src/main/java/org/example/admin/controller/AdminController.java
+++ src/main/java/org/example/admin/controller/AdminController.java
@@ -3,10 +3,7 @@ package org.example.admin.controller;
 import org.example.admin.model.Admin;
 import org.example.admin.service.AdminService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +22,7 @@ public class AdminController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Admin>> findAdmin() {
+    public ResponseEntity<List<Admin>> findAllAdmins() {
         return ResponseEntity.ok(adminService.findAll());
     }
 
